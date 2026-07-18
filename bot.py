8670bd3ac29ad2ca034c51a97b2aac93788032fa
lu6na7 <zelenymivolosami80@gmail.com>
2026-07-18 19:12:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ from telegram.ext import (
     filters,
 )
 
-TOKEN = "СЮДА_ВСТАВЬ_ТОКЕН_ОТ_BOTFATHER"
+TOKEN = "8929108765:AAEpt_OZIrSAa9djKKz4JJRHV4X0IUyNA1w"
 
 ROLE, AGE, REASON = range(3)
 
-ADMIN_ID = 123456789
+ADMIN_ID = -1004389081655
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 Привет! Для подачи заявки ответь на несколько вопросов.\n\n🎭 Какая у тебя роль?")
