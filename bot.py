4411eddce0418290c39f426fe0d6b6875d03c9d5
lu6na7 <zelenymivolosami80@gmail.com>
2026-07-18 20:18:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,5 +15,37 @@ ROLE, AGE, REASON = range(3)
 ADMIN_ID = -1004389081655
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("👋 Привет! Для подачи заявки ответь на несколько вопросов.\n\n🎭 Какая у тебя роль?")
+    await update.message.reply_text("👋☺️ Привет! Для подачи заявки ответь на несколько вопросов.")
     return ROLE
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("1️⃣ За какую роль хотите вступить?")
+    return ROLE
+
+
+async def age(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["role"] = update.message.text
+    await update.message.reply_text(
+        "2️⃣ Сколько вам лет?\n📎 Пришлите доказательство возраста."
+    )
+    return AGE
+
+
+async def username(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["age"] = update.message.text
+    await update.message.reply_text("3️⃣ Ваш Telegram-юз (@username)?")
+    return REASON
+
+
+async def finish(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["username"] = update.message.text
+
+    text = (
+        "📩 Новая заявка\n\n"
+        f"🎭 Роль: {context.user_data['role']}\n"
+        f"🎂 Возраст: {context.user_data['age']}\n"
+        f"👤 Юз: {context.user_data['username']}"
+    )
+
+    await context.bot.send_message(chat_id=ADMIN_ID, text=text)
+    await update.message.reply_text("✅ Ваша заявка успешно отправлена!")
+    return ConversationHandler.END
